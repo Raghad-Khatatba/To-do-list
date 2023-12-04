0efbf911b68a28b6fa3c006fe03a35314f41c9f0
RaghadKhatatba <RaghadZKhatatba@gmail.com>
2023-12-04 16:26:20 +0200

    add popup

--- react-to-do-list/src/Component/board/board.jsx
+++ react-to-do-list/src/Component/board/board.jsx
@@ -1,17 +1,33 @@
 import React, { useState } from "react";
 import "./board.css";
 import Card from "../Card/Card";
+import Popup from "../Poupup/Poupup";
 
 export default function Board({}) {
   const [todos, setTodos] = useState([]);
-
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
   const addTodo = (task) => {
     setTodos([...todos, { task }]);
   };
+
+  const openPopup = () => {
+    setIsPopupOpen(true);
+  };
+
+  const closePopup = () => {
+    setIsPopupOpen(false);
+  };
+
   return (
     <div className="Board">
       <h1>Get Things Done !</h1>
-      <button className="Todo-btn">Add Task</button>
+      <button className="Todo-btn" onClick={openPopup}>
+        Add Task
+      </button>
+      {isPopupOpen && (
+        <Popup  onClose={closePopup} addTodo={addTodo} />
+      )}
+
       {todos.map((task, index) => (
         <Card key={index} task={task} />
       ))}
--- react-to-do-list/src/Component/poupup/poupup.jsx
+++ react-to-do-list/src/Component/poupup/poupup.jsx
@@ -1,7 +1,34 @@
-import React from 'react'
+// popup.js
+import React, { useState } from "react";
+import "./poupup.css";
 
-export default function poupup() {
+export default function Popup({ onClose, addTodo }) {
+  const [task, setTask] = useState("");
+
+  const handleAddTask = () => {
+    addTodo(task);
+    onClose();
+  };
+
   return (
-    <div>poupup</div>
-  )
+    <div className="Popup-overlay">
+      <div className="Popup-content">
+        <input
+          type="text"
+          value={task}
+          onChange={(e) => setTask(e.target.value)}
+          className="Todo-input"
+          placeholder="What is the task today?"
+        />
+        <div>
+          <button className="Todo-btn" onClick={onClose}>
+            Cancel
+          </button>
+          <button className="Todo-btn" onClick={handleAddTask}>
+            Add Task
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
