b06c9f05cc5c6f36117af6011edb51687437b7e1
RaghadKhatatba <RaghadZKhatatba@gmail.com>
2023-12-05 16:25:28 +0200

    add card

--- react-to-do-list/src/Component/card/card.jsx
+++ react-to-do-list/src/Component/card/card.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, { useState } from "react";
+import "./card.css";
 
-export default function card() {
+export default function Card({ task }) {
+  const [isCompleted, setIsCompleted] = useState(false);
+
+  const toggleCompleted = () => {
+    setIsCompleted(!isCompleted);
+  };
+
   return (
-    <div>card</div>
-  )
+    <div
+      className={`Todo ${isCompleted ? "Completed" : "Incompleted"}`}
+      onClick={toggleCompleted}
+    >
+      <p>{task.task}</p>
+    </div>
+  );
 }
